036d47ed255dcbdda3dc8e0be36149d24b219f4a
Eric Castillo <erictomas27@hotmail.com>
2024-12-20 00:15:40 +0000

    initial layout

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,18 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar navbar-light" style={{backgroundColor: '#3E362E'}}>
 			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
+				<span className="h3 mb-0" style={{color: "#EBE9E1"}}>Legal Sync</span>
 			</div>
 		</nav>
 	);
