ad607e9784319765ab7b04c1169d78085e61ea86
Rebekah Banks <168665115+Banksrm75@users.noreply.github.com>
2024-12-20 22:52:23 +0000

    models.py and routes.py updated

--- src/api/models.py
+++ src/api/models.py
@@ -15,5 +15,42 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            
             # do not serialize the password, its a security breach
+        }
+    
+class Clients(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(250), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    phone_number = db.Column(db.String(15), unique=True, nullable=False)
+    
+    def __repr__(self):
+        return '<Client %r>' % self.full_name
+
+    def serialize(self):
+        return {
+            # do not serialize the password, its a security breach
+            "full_name": self.full_name,
+            "email": self.email,
+            "phone_number": self.phone_number
+        }
+    
+class Lawyers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(250), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    phone_number = db.Column(db.String(15), unique=True, nullable=False)
+    
+    def __repr__(self):
+        return '<Lawyer %r>' % self.full_name
+
+    def serialize(self):
+        return {
+            # do not serialize the password, its a security breach
+            "full_name": self.full_name,
+            "email": self.email,
+            "phone_number": self.phone_number
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -20,3 +20,49 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+# GET all CLIENTS
+@api.route("/clients", methods=['GET'])
+def get_all_clients():
+
+    all_clients = Clients.query.all()
+
+    if all_clients is None:
+        return jsonify("No records found!"), 404
+    else:
+        all_clients = list(map(lambda x: x.serialize(), all_clients))
+        return jsonify(all_clients), 200
+
+# GET a specific client
+@api.route("/clients/<int:id>", methods=["GET"])
+def get_client(id):
+    
+    client = Clients.query.get(id)
+
+    if client is None:
+        raise APIException(f'Client ID {id} is not found!', status_code = 404)
+    client = client.serialize()
+    return jsonify(client), 200
+
+# GET all LAWYERS
+@api.route("/lawyers", methods=['GET'])
+def get_all_lawyers():
+
+    all_lawyers = Lawyers.query.all()
+
+    if all_lawyers is None:
+        return jsonify("No records found!"), 404
+    else:
+        all_lawyers = list(map(lambda x: x.serialize(), all_lawyers))
+        return jsonify(all_lawyers), 200
+
+# GET a specific lawyer
+@api.route("/lawyers/<int:id>", methods=["GET"])
+def get_lawyer(id):
+    
+    lawyer = Lawyers.query.get(id)
+
+    if lawyer is None:
+        raise APIException(f'Lawyer ID {id} is not found!', status_code = 404)
+    lawyer = lawyer.serialize()
+    return jsonify(lawyer), 200
