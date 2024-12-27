f7e4319f287978a27c83ddfa33f4757bdf491b02
Rebekah Banks <168665115+Banksrm75@users.noreply.github.com>
2024-12-27 18:33:46 +0000

    POST new contacts to database

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -7,6 +7,32 @@ const getState = ({ getStore, getActions, setStore }) => {
 		actions: {
 			signup: async (e, {name, email, password, phone, address})=>{
 				e.preventDefault();
+
+				let newContact = {
+					name: name,
+					email: email,
+					password: password,
+					phone: phone,
+					address: address
+				}
+
+				let options = {
+					method: 'POST',
+					body: JSON.stringify(newContact), 
+					headers: {
+						'Content-Type': 'application/json'
+					}
+				}
+
+				fetch('https://reimagined-halibut-4jq4pxvqr7963jj5r-3000.app.github.dev', options)
+				.then(response => {
+					if (!response.ok) {
+						throw Error("Error. Unable to post new contact.");
+					}
+					console.log("Contact successfully added!");
+					return response.json();
+				})
+				.catch(error => {console.log("More info on error: ", error)})
 			}
 		}
 	};
