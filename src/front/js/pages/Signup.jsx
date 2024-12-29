857decb6bcab97873ad98d387b8ff4df1ba15091
Eric Castillo <erictomas27@hotmail.com>
2024-12-29 20:44:13 +0000

    basic structure for lawyer signup

--- src/front/js/pages/Signup.jsx
+++ src/front/js/pages/Signup.jsx
@@ -3,6 +3,7 @@ import { CommonFields } from "../component/signup";
 import { Context } from "../store/appContext"
 
 const Signup = () => {
+  const [userType, setUserType] = React.useState("client")
   const [userData, setUserData] = React.useState({ name: '', email: '', password: '', phone: '', address: '' })
   const { actions } = React.useContext(Context)
 
@@ -16,16 +17,44 @@ const Signup = () => {
     setUserData(prev => ({ ...prev, [name]: value }))
   }
 
+  const changeUserType = (e) => {
+    const { value } = e.target
+    setUserType(value)
+  }
+
   return (
     <>
-      <div className="form-check form-switch">
-          <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-          <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
+      <div style={{ margin: "30px 0px 20px 0px" }} className="form-check form-switch d-flex justify-content-center">
+        <input
+          onChange={changeUserType}
+          style={{ width: "45px", height: "30px" }}
+          className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"
+          value={"client"}
+          checked={userType === "client"}
+        />
+
+        <label class="form-check-label" for="flexSwitchCheckDefault">Client</label>
+
+        <input
+          onChange={changeUserType}
+          style={{ marginLeft: "20px", width: "45px", height: "30px" }}
+          className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefaults"
+          value={"lawyer"}
+          checked={userType === "lawyer"}
+        />
+
+        <label class="form-check-label" for="flexSwitchCheckDefault">Lawyer</label>
       </div>
 
-      <form onSubmit={(e) => (actions.signup(e, userData))} className="m-auto border border-1" style={{ width: "400px" }}>
-        <CommonFields userData={userData} handleChange={handleChange} />
-      </form>
+      {userType === "client" ?
+        <form onSubmit={(e) => (actions.signupClient(e, userData))} className="m-auto border border-1" style={{ width: "400px" }}>
+          <CommonFields userData={userData} handleChange={handleChange} />
+        </form>
+        :
+        <form onSubmit={(e) => (actions.signupClient(e, userData))} className="m-auto border border-1" style={{ width: "400px" }}>
+          <CommonFields userData={userData} handleChange={handleChange} />
+        </form>
+      }
     </>
 
   );
