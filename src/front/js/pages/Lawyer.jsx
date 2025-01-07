fd7224324ec39a02beb89dea4004cbd04188fedc
JC-kip <jjcasasm16@gmail.com>
2025-01-07 02:06:05 +0000

    Layer dashboard

--- src/front/js/pages/Lawyer.jsx
+++ src/front/js/pages/Lawyer.jsx
@@ -23,26 +23,111 @@ export const ProtectedLawyer = ({ children }) => {
       }
     </>
   )
-}
+};
+
+const clients = [
+  {
+    name: 'Eric',
+    status: 'Pending',
+    time: '10:00 AM',
+    type: 'Initial Consultation'
+  },
+  {
+    name: 'Rebekah',
+    status: 'Confirmed',
+    time: '1:00 PM',
+    type: 'Follow-up'
+  },
+  {
+    name: 'Jose',
+    status: 'In Progress',
+    time: '3:00 PM',
+    type: 'Document Review'
+  }
+];
 
 const Lawyer = () => {
   const navigate = useNavigate()
-  const links = [{to: '/profile', text: 'Profile'}]
+  const links = [{ to: '/lawyer', text: 'Dashboard' }, { to: '/profile', text: 'Profile' }, { to: '/lawyer', text: 'Incoming' }]
 
   return (
     <>
-      <ul className='w-50 m-auto text-center'>
+      <div className='w-50 m-auto text-center'>
         {
-          links.map((link, index)=>{
+          links.map((link, index) => {
             return <li className='list-unstyled h3' key={index}><Link className='text-decoration-none' to={link.to}>{link.text}</Link></li>
           })
         }
-      </ul>
-
-      <div className='m-auto' style={{border: "1px solid black", width: "750px"}}>
-        kksks
       </div>
+
+      {/* <div className='m-auto' style={{border: "1px solid black", width: "750px"}}>
+        kksks
+      </div> */}
+
+      <div className="container py-4">
+        {/* Header Section */}
+        <div className="row mb-4">
+          <div className="col">
+            <h2 className="mb-3">Dashboard</h2>
+            <div className="d-flex gap-3">
+              <div className="card flex-grow-1">
+                <div className="card-body text-center">
+                  <h5 className="card-title">Today's Appointments</h5>
+                  <h3 className="mb-0">{clients.length}</h3>
+                </div>
+              </div>
+              <div className="card flex-grow-1">
+                <div className="card-body text-center">
+                  <h5 className="card-title">Incoming</h5>
+                  <h3 className="mb-0">0</h3>
+                </div>
+              </div>
+              <div className="card flex-grow-1">
+                <div className="card-body text-center">
+                  <h5 className="card-title">Active Cases</h5>
+                  <h3 className="mb-0">{clients.length}</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Clients Section */}
+        <div className="row">
+          <div className="col">
+            <div className="card">
+              <div className="card-header bg-light">
+                <h4 className="mb-0">Incoming Appointments</h4>
+              </div>
+              <div className="card-body">
+                <div className="table-responsive">
+                  <table className="table table-hover">
+                    <thead>
+                      <tr>
+                        <th>Client Name</th>
+                        <th>Time</th>
+                        <th>Type</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {clients.map((client, index) => (
+                        <tr key={index}>
+                          <td>{client.name}</td>
+                          <td>{client.time}</td>
+                          <td>{client.type}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </>
+
   )
 }
 
@@ -51,8 +136,8 @@ export const Profile = () => {
   const state = useLocation().state
   const params = new URLSearchParams(location.search)
   const lawyerId = params.get('id')
-  const lawyer = JSON.parse(localStorage.getItem("lawyers"))?.find(obj=>String(obj.id) === lawyerId)
-  const [photo, setPhoto] = React.useState(()=>{
+  const lawyer = JSON.parse(localStorage.getItem("lawyers"))?.find(obj => String(obj.id) === lawyerId)
+  const [photo, setPhoto] = React.useState(() => {
     const storedPhoto = localStorage.getItem("Profile Picture")
     return storedPhoto === "null" ? null : storedPhoto
   })
@@ -66,14 +151,14 @@ export const Profile = () => {
 
 
   return (
-      <div>
-        <input onChange={handlePhotoChange} type='file' name='file' accept='image/*' className='d-none' id='profile-picture' />
+    <div>
+      <input onChange={handlePhotoChange} type='file' name='file' accept='image/*' className='d-none' id='profile-picture' />
 
-        <label style={{ cursor: "pointer", margin: "15px 0px 0px 90px" }} htmlFor='profile-picture'>
-          <img className='border' style={{ height: "200px", width: "200px" }} src={state?.photo || lawyer?.photo || photo || `/profile-picture-placeholder.jpg`}></img>
-        </label>
+      <label style={{ cursor: "pointer", margin: "15px 0px 0px 90px" }} htmlFor='profile-picture'>
+        <img className='border' style={{ height: "200px", width: "200px" }} src={state?.photo || lawyer?.photo || photo || `/profile-picture-placeholder.jpg`}></img>
+      </label>
 
-      </div>
+    </div>
   )
 }
 
