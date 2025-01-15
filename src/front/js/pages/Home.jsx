cbdb60349deb13ed1c190fbacf4516bd869eaf48
Rebekah Banks <168665115+Banksrm75@users.noreply.github.com>
2025-01-15 23:15:20 +0000

    Fixing Text Size and Alignment

--- src/front/js/pages/Home.jsx
+++ src/front/js/pages/Home.jsx
@@ -6,21 +6,20 @@ import backgroundImage from '../../../../public/LandingPage.jpg';
 export const Home = () => {
     return (
         <>
-            <div 
+            <div    
                 style={{ 
                     backgroundImage: `url(${backgroundImage})`,
                     backgroundSize: 'cover', 
                     backgroundPosition: 'center', 
                     minHeight: '100vh' 
-                }}
-            >
-                <div style={{ padding: "30px", margin: "0px 30px 0px auto", height: "200px", width: "1000px"}}>
-                    Legal Sync is a streamlined platform designed to connect clients with the legal expertise they need. By helping users identify their specific legal requirements, our website simplifies the process of finding the right lawyer. Clients can explore various areas of law, from family matters to corporate issues, and match with experienced attorneys from our extensive database. Each lawyer in our network has clearly identified their areas of specialty, ensuring a tailored and effective solution for every legal challenge. Whether you're seeking advice, representation, or consultation, Legal Sync makes finding qualified legal assistance quick, easy, and reliable.
+                }}>
+                <div  style={{ padding: "30px", margin: "0px 70px 0px auto", height: "200px", width: "800px"}}>
+                    <p className='fs-3'>Legal Sync is a streamlined platform designed to connect clients with the legal expertise they need. By helping users identify their specific legal requirements, our website simplifies the process of finding the right lawyer. Clients can explore various areas of law, from family matters to corporate issues, and match with experienced attorneys from our extensive database. Each lawyer in our network has clearly identified their areas of specialty, ensuring a tailored and effective solution for every legal challenge. Whether you're seeking advice, representation, or consultation, Legal Sync makes finding qualified legal assistance quick, easy, and reliable.</p>
                </div>
                 
-                <div style={{ padding: "30px", margin: "0px 30px 0px auto", height: "200px", width: "1000px"}}>
-                    <div><label className="m-auto">Don't have an account? <Link className="text-decoration-none" to={"/signup"} state={{ userType: "Client" }}>Sign up</Link></label></div>
-                    <div><label className="m-auto">Need to login? <Link className="text-decoration-none" to={"/login"} state={{ userType: "Client" }}>Log In</Link></label></div>
+                <div className="fs-4" style={{ padding: "30px", margin: "320px 70px 0px auto", height: "200px", width: "800px"}}>
+                    <div><label>Don't have an account? <Link className="text-decoration-none" to={"/signup"} state={{ userType: "Client" }}>Sign up</Link></label></div>
+                    <div><label>Need to login? <Link className="text-decoration-none" to={"/login"} state={{ userType: "Client" }}>Log In</Link></label></div>
                 </div>
             </div>
             
